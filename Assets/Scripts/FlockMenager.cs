f52b6ca2277395f2c79324a8ede9e79274b7d4a7
Diego Vallina <a45933@alunos.ipb.pt>
2025-10-16 23:13:54 +0100

    16/10/2025
    Tentei alterar o código do Tutorial, mas os peixes entenderam que o limite do aquario era a distância entre um e outro

--- Assets/Scripts/FlockMenager.cs
+++ Assets/Scripts/FlockMenager.cs
@@ -1,24 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Não está igual ao código original pois deu erro e eu tive de tirar as Listas para funcionar
 public class FlockMenager : MonoBehaviour
 {
-    public GameObject = fishPrefab;
+    public GameObject allFish ;
     public int numFish = 20;
-    public GameObject[] = allFish;
+    
     public Vector3 swimLimits = new Vector3(5, 5, 5);
 
     // Start is called before the first frame update
     void Start()
     {
-        allFish = new GameObject[numFish];
+        //allFish = new GameObject[numFish];
         for (int i = 0; i < numFish; i++)
         {
-            Vector3 pos - this.transform.position + new Vector3(Random.Range(-swinLimits.x, swinLimits.x)
-                                                                Random.Range(-swinLimits.y, swinLimits.y)
-                                                                Random.Range(-swinLimits.z, swinLimits.z));
-            allFish[i] = Instantiate(fishPrefab, pos, Quaternion.identity);
+            
+            //Mexer os peixes - com limits 
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x), Random.Range(-swimLimits.y, swimLimits.y), Random.Range(-swimLimits.z, swimLimits.z));
+            allFish = Instantiate(allFish, pos, Quaternion.identity);
         }
 
     }
