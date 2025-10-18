b9774a5599cb805c919bf227001a8ee771739e9d
Diego Vallina <a45933@alunos.ipb.pt>
2025-10-18 15:45:38 +0100

    17/10/2025
    Ta meio bagunçado, mas só usar o original que agora ta tudo bem

--- Assets/Scripts/Flock.cs
+++ Assets/Scripts/Flock.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlockMenager(Original) : MonoBehaviour
+public class Flock : MonoBehaviour
 {
+    float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = Random.Range(FlockManager_Original.FM.minSpeed, FlockManager_Original.FM.maxSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
